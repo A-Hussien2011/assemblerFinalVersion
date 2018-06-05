d06da1574eebd54b68278f864b543982d7e71640
AlaaShehab <alaa.shehab.95@gmail.com>
2018-06-05 15:41:22 +0200

    edits in format class (added 2 setters)

--- pass2/include/Format.h
+++ pass2/include/Format.h
@@ -1,6 +1,8 @@
 #ifndef FORMAT_H
 #define FORMAT_H
+#include <string>
 
+using namespace std;
 
 class Format
 {
@@ -13,6 +15,8 @@ class Format
         void setXflag(bool);
         void setPflag(bool);
         void setBflag(bool);
+        void setRegister1(int);
+        void setRegister2(int);
         void setDispalcement(string);
         void setOperationCode(string);
         void setFormateType(int);
@@ -22,6 +26,7 @@ class Format
     private:
         ///set ur getters
         ///set booleans for all flags + format + strings
+        ///set variables
 
 };
 
